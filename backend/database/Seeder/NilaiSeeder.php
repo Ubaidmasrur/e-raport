7ce748324621df887867ec0c7dcccf99db524bf2
Ubaidmasrur <66166577+Ubaidmasrur@users.noreply.github.com>
2025-07-11 22:45:04 +0700

    Update NilaiSeeder.php

--- backend/database/Seeder/NilaiSeeder.php
+++ backend/database/Seeder/NilaiSeeder.php
@@ -1,28 +1,12 @@
-namespace Database\Seeders;
-
-use Illuminate\Database\Seeder;
-use App\Models\Nilai;
-use App\Models\Siswa;
-use App\Models\Indikator;
-
-class NilaiSeeder extends Seeder
-{
-    public function run(): void
-    {
-        $siswas = Siswa::all();
-        $indikators = Indikator::all();
-
-        foreach ($siswas as $siswa) {
-            foreach ($indikators as $indikator) {
-                Nilai::create([
-                    'siswa_id'     => $siswa->id,
-                    'indikator_id' => $indikator->id,
-                    'semester'     => 'Ganjil',
-                    'tahun_ajaran' => '2024/2025',
-                    'nilai'        => rand(70, 95),
-                    'komentar'     => 'Cukup baik dan terus berkembang'
-                ]);
-            }
-        }
+foreach (Siswa::all() as $siswa) {
+    foreach (Indikator::all() as $indikator) {
+        Nilai::create([
+            'siswa_id' => $siswa->id,
+            'indikator_id' => $indikator->id,
+            'semester' => 'Ganjil',
+            'tahun_ajaran' => '2024/2025',
+            'nilai' => rand(75, 95),
+            'komentar' => 'Baik dan terus meningkat',
+        ]);
     }
 }
