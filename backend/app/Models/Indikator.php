7a756a32d72c4c16267e8852fe99422ada928bbb
Ubaidmasrur <66166577+Ubaidmasrur@users.noreply.github.com>
2025-07-11 22:30:45 +0700

    Update Indikator.php

--- backend/app/Models/Indikator.php
+++ backend/app/Models/Indikator.php
@@ -1,8 +1,10 @@
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
 class Indikator extends Model
 {
-    use HasFactory;
-
-    protected $fillable = ['nama', 'domain'];
+    protected $fillable = ['nama', 'domain']; // domain: kognitif, afektif, psikomotorik
 
     public function nilais()
     {
