4678981c63f21f29f4c36b6624433d63ddf13290
Ubaidmasrur <66166577+Ubaidmasrur@users.noreply.github.com>
2025-07-11 22:01:23 +0700

    Create StoreNilaiRequest.php

---
+++ backend/app/Http/Request/StoreNilaiRequest.php
@@ -0,0 +1,23 @@
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreNilaiRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'siswa_id'     => 'required|exists:siswas,id',
+            'indikator_id' => 'required|exists:indikators,id',
+            'semester'     => 'required|string',
+            'tahun_ajaran' => 'required|string',
+            'nilai'        => 'required|integer|min:0|max:100',
+            'komentar'     => 'nullable|string'
+        ];
+    }
+}
