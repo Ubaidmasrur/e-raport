ec5e4d08f6579cfc1ce8ab572873ddcd24feb3d9
Ubaidmasrur <66166577+Ubaidmasrur@users.noreply.github.com>
2025-07-11 22:00:48 +0700

    Create UpdateSiswaRequest.php

---
+++ backend/app/Http/Request/UpdateSiswaRequest.php
@@ -0,0 +1,20 @@
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateSiswaRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'nama'    => 'sometimes|string|max:100',
+            'kelas'   => 'sometimes|string|max:10',
+            'user_id' => 'sometimes|exists:users,id'
+        ];
+    }
+}
