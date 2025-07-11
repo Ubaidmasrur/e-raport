4fb5a6ed5431c4ab99474ab4886e740d97db0ed2
Ubaidmasrur <66166577+Ubaidmasrur@users.noreply.github.com>
2025-07-11 22:00:59 +0700

    Create StoreIndikatorRequest.php

---
+++ backend/app/Http/Request/StoreIndikatorRequest.php
@@ -0,0 +1,19 @@
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreIndikatorRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'nama'   => 'required|string|max:100',
+            'domain' => 'required|in:kognitif,afektif,psikomotorik'
+        ];
+    }
+}
