102e849e50531f1490e85b0e50a0ad077b277758
Ubaidmasrur <66166577+Ubaidmasrur@users.noreply.github.com>
2025-07-11 22:31:16 +0700

    Update TahunAjaran.php

--- backend/app/Models/TahunAjaran.php
+++ backend/app/Models/TahunAjaran.php
@@ -6,10 +6,9 @@ class TahunAjaran extends Model
 {
     protected $fillable = ['tahun', 'is_active'];
 
-    // Hanya satu tahun ajaran yang aktif
     public static function setActive($id)
     {
         self::query()->update(['is_active' => false]);
-        self::find($id)->update(['is_active' => true]);
+        self::findOrFail($id)->update(['is_active' => true]);
     }
 }
